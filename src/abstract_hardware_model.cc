2c8e2c49f9b9410df61e85ec7e2d9471e04ac6cb
oliver1325 <guyo@oregonstate.edu>
2017-08-31 15:06:26 -0700

    Signed-off-by: oliver1325 <guyo@oregonstate.edu>

--- src/abstract_hardware_model.cc
+++ src/abstract_hardware_model.cc
@@ -1,11 +1,7 @@
-// Copyright (c) 2009-2011, Tor M. Aamodt, Inderpreet Singh, Timothy Rogers,
-// The University of British Columbia
-// All rights reserved.
-//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
-// Redistributions of source code must retain the above copyright notice, this
+// aaaaaa of source code must retain the above copyright notice, this
 // list of conditions and the following disclaimer.
 // Redistributions in binary form must reproduce the above copyright notice, this
 // list of conditions and the following disclaimer in the documentation and/or
