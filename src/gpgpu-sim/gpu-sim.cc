07fee2d6a30d01a89d70b1cceeab0ace21d20e41
oliver1325 <guyo@oregonstate.edu>
2017-09-01 15:52:05 -0700

    Signed-off-by: oliver1325 <guyo@oregonstate.edu>

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -81,6 +81,7 @@ bool g_interactive_debugger_enabled = false;
 
 unsigned long long  gpu_sim_cycle = 0;
 unsigned long long  gpu_tot_sim_cycle = 0;
+int core_numbers = 0;
 
 
 // performance counter for stalls due to congestion.
@@ -568,6 +569,7 @@ gpgpu_sim::gpgpu_sim( const gpgpu_sim_config &config )
 
 
   m_cluster = new simt_core_cluster*[m_shader_config->n_simt_clusters];
+  core_numbers = m_shader_config->n_simt_clusters;
   for (unsigned i = 0; i < m_shader_config->n_simt_clusters; i++)
     m_cluster[i] = new simt_core_cluster(this, i, m_shader_config, m_memory_config, m_shader_stats, m_memory_stats);
 
