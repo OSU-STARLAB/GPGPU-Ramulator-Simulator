77f46bb5976121d3c7f07bb4226ddb1f00fe5ef5
oliver1325 <guyo@oregonstate.edu>
2017-10-10 16:02:44 -0700

    Signed-off-by: oliver1325 <guyo@oregonstate.edu>

--- src/ramulator_sim/Memory.h
+++ src/ramulator_sim/Memory.h
@@ -301,7 +301,7 @@ public:
                 req.addr_vec[i] = slice_lower_bits(addr, addr_bits[i]);
             break;
         case int(Type::RoBaRaCoCh):
-            //fprintf(stderr, "the addr bits is %d\n", addr_bits[0]);
+            fprintf(stderr, "the addr bits is %d, the address last is %d\n", addr_bits[0], addr_bits[addr_bits.size() - 1]);
             req.addr_vec[0] = slice_lower_bits(addr, addr_bits[0]);
             req.addr_vec[addr_bits.size() - 1] = slice_lower_bits(addr, addr_bits[addr_bits.size() - 1]);
 
